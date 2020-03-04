9d9ca428e92143657b8112384226201eec4016d8
bartek <bjakubov@gmail.com>
2020-03-04 18:12:32 +0100

    WIP: Add basic parsing

--- frontend/src/containers/Layout.jsx
+++ frontend/src/containers/Layout.jsx
@@ -13,6 +13,66 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+const dataToCities = ({ data }) => {
+  const cities = {};
+
+  data.map(d => checkAgeAndSetKey(cities, d.location));
+  // console.log(cities);
+
+  return cities;
+};
+
+const dataToJobs = ({ data }) => {
+  const jobs = {};
+
+  data.map(d => {
+
+  });
+
+  return jobs;
+};
+
+const checkAgeAndSetKey = (obj, key) => {
+  if (!obj[key]) {
+    obj[key] = 1;
+  } else {
+    obj[key]++;
+  }
+};
+
+const mapAge = ({ data }) => {
+  const age = {};
+
+  data.map(d => {
+    switch (true) {
+      case d.age <= 20:
+        checkAgeAndSetKey(age, '<=20');
+        break;
+      case d.age >= 21 && d.age <= 25:
+        checkAgeAndSetKey(age, '21-25');
+        break;
+      case d.age >= 26 && d.age <= 30:
+        checkAgeAndSetKey(age, '26-30');
+        break;
+      case d.age >= 31 && d.age <= 35:
+        checkAgeAndSetKey(age, '31-35');
+        break;
+      case d.age >= 36 && d.age <= 40:
+        checkAgeAndSetKey(age, '36-40');
+        break;
+      case d.age >= 41 && d.age <= 45:
+        checkAgeAndSetKey(age, '41-45');
+        break;
+      default:
+        checkAgeAndSetKey(age, '>46');
+        break;
+    };
+    return '';
+  });
+
+  return age;
+}
+
 const parseTest = () => {
   const csvFilePath = require("../data/data_test.csv");
   const Papa = require("papaparse/papaparse.min");
@@ -21,7 +81,10 @@ const parseTest = () => {
     header: true,
     download: true,
     skipEmptyLines: true,
-    complete: (res) => console.log('results:', res)
+    complete: (res) => {
+      mapAge(res);
+      dataToCities(res)
+    }
   });
 }
 

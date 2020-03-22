2607ebbb7147ff1432c11591a796b8f906dc93fb
Bartosz Jakubowski <bjakubov@gmail.com>
2020-03-22 08:28:42 +0100

    feat: Add basic pie charts with data from csv

--- frontend/src/components/PieChart.jsx
+++ frontend/src/components/PieChart.jsx
@@ -1,53 +1,21 @@
 import { ResponsivePie } from '@nivo/pie';
 import React from 'react';
 
-const dataExample = [
-  {
-    id: "php",
-    label: "php",
-    value: 287,
-    color: "hsl(297, 70%, 50%)"
-  },
-  {
-    id: "haskell",
-    label: "haskell",
-    value: 336,
-    color: "hsl(65, 70%, 50%)"
-  },
-  {
-    id: "lisp",
-    label: "lisp",
-    value: 182,
-    color: "hsl(135, 70%, 50%)"
-  },
-  {
-    id: "sass",
-    label: "sass",
-    value: 50,
-    color: "hsl(43, 70%, 50%)"
-  },
-  {
-    id: "go",
-    label: "go",
-    value: 247,
-    color: "hsl(199, 70%, 50%)"
-  }
-];
-
-const PieChart = ({ data }) => (
+const PieChart = ({ data, theme }) => (
   <ResponsivePie
-    data={dataExample}
+    data={data}
+    sortByValue={true}
     margin={{
-      top: 40,
-      right: 80,
-      bottom: 80,
-      left: 80
+      top: 30,
+      right: 60,
+      bottom: 60,
+      left: 60
     }}
-    innerRadius={0}
+    innerRadius={0.4}
     padAngle={0.7}
     cornerRadius={3}
     colors={{
-      scheme: "nivo"
+      scheme: theme
     }}
     borderWidth={1}
     borderColor={{
@@ -55,7 +23,7 @@ const PieChart = ({ data }) => (
       modifiers: [["darker", 0.2]]
     }}
     radialLabelsSkipAngle={10}
-    radialLabelsTextXOffset={6}
+    radialLabelsTextXOffset={4}
     radialLabelsTextColor="#333333"
     radialLabelsLinkOffset={0}
     radialLabelsLinkDiagonalLength={16}
@@ -69,96 +37,6 @@ const PieChart = ({ data }) => (
     animate={true}
     motionStiffness={90}
     motionDamping={15}
-    defs={[
-      {
-        id: "dots",
-        type: "patternDots",
-        background: "inherit",
-        color: "rgba(255, 255, 255, 0.3)",
-        size: 4,
-        padding: 1,
-        stagger: true
-      },
-      {
-        id: "lines",
-        type: "patternLines",
-        background: "inherit",
-        color: "rgba(255, 255, 255, 0.3)",
-        rotation: -45,
-        lineWidth: 6,
-        spacing: 10
-      }
-    ]}
-    fill={[
-      {
-        match: {
-          id: "ruby"
-        },
-        id: "dots"
-      },
-      {
-        match: {
-          id: "c"
-        },
-        id: "dots"
-      },
-      {
-        match: {
-          id: "go"
-        },
-        id: "dots"
-      },
-      {
-        match: {
-          id: "python"
-        },
-        id: "dots"
-      },
-      {
-        match: {
-          id: "scala"
-        },
-        id: "lines"
-      },
-      {
-        match: {
-          id: "lisp"
-        },
-        id: "lines"
-      },
-      {
-        match: {
-          id: "elixir"
-        },
-        id: "lines"
-      },
-      {
-        match: {
-          id: "javascript"
-        },
-        id: "lines"
-      }
-    ]}
-    legends={[
-      {
-        anchor: "bottom",
-        direction: "row",
-        translateY: 56,
-        itemWidth: 100,
-        itemHeight: 18,
-        itemTextColor: "#999",
-        symbolSize: 18,
-        symbolShape: "circle",
-        effects: [
-          {
-            on: "hover",
-            style: {
-              itemTextColor: "#000"
-            }
-          }
-        ]
-      }
-    ]}
   />
 );
 
--- frontend/src/containers/Layout.jsx
+++ frontend/src/containers/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Paper } from "@material-ui/core";
 import PieChart from "../components/PieChart";
+import useParseCsvToChartData from "../hooks/useParseCsvToChartData";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,113 +15,52 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const dataToCities = ({ data }) => {
-  const cities = {};
-
-  data.map(d => checkAgeAndSetKey(cities, d.location));
-  // console.log(cities);
-
-  return cities;
-};
-
-const dataToJobs = ({ data }) => {
-  const jobs = {};
-
-  data.map(d => {
-
-  });
-
-  return jobs;
-};
-
-const checkAgeAndSetKey = (obj, key) => {
-  if (!obj[key]) {
-    obj[key] = 1;
-  } else {
-    obj[key]++;
-  }
-};
-
-const mapAge = ({ data }) => {
-  const age = {};
-
-  data.map(d => {
-    switch (true) {
-      case d.age <= 20:
-        checkAgeAndSetKey(age, '<=20');
-        break;
-      case d.age >= 21 && d.age <= 25:
-        checkAgeAndSetKey(age, '21-25');
-        break;
-      case d.age >= 26 && d.age <= 30:
-        checkAgeAndSetKey(age, '26-30');
-        break;
-      case d.age >= 31 && d.age <= 35:
-        checkAgeAndSetKey(age, '31-35');
-        break;
-      case d.age >= 36 && d.age <= 40:
-        checkAgeAndSetKey(age, '36-40');
-        break;
-      case d.age >= 41 && d.age <= 45:
-        checkAgeAndSetKey(age, '41-45');
-        break;
-      default:
-        checkAgeAndSetKey(age, '>46');
-        break;
-    };
-    return '';
-  });
-
-  return age;
-};
-
-const parseTest = () => {
-  const csvFilePath = require("../data/data_test.csv");
-  const Papa = require("papaparse/papaparse.min");
-
-  Papa.parse(csvFilePath, {
-    header: true,
-    download: true,
-    skipEmptyLines: true,
-    complete: (res) => {
-      mapAge(res);
-      dataToCities(res)
-    }
-  });
-};
-
 const Layout = () => {
   const classes = useStyles();
-  parseTest();
+  const [ age, cities, contract, stack ] = useParseCsvToChartData();
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={12} md={4}>
-          <Paper className={classes.paper}>1</Paper>
-        </Grid>
-        <Grid item xs={12} sm={12} md={4}>
-          <Paper className={classes.paper}>2</Paper>
-        </Grid>
-        <Grid item xs={12} sm={12} md={4}>
-          <Paper className={classes.paper}>3</Paper>
-        </Grid>
-        <Grid item xs={12} sm={12} md={8}>
-          <Paper className={classes.paper} style={{ height: 500 }}>
-            <PieChart />
+        <Grid item xs={12} sm={12} md={6}>
+          <Paper className={classes.paper} style={{ height: 450 }}>
+            {cities.length &&
+            <PieChart
+              data={stack}
+              theme="yellow_green_blue"
+            />
+            }
           </Paper>
         </Grid>
-        <Grid item xs={12} sm={12} md={4}>
-          <Paper className={classes.paper}>5</Paper>
+        <Grid item xs={12} sm={12} md={6}>
+          <Paper className={classes.paper} style={{ height: 450 }}>
+            {cities.length &&
+              <PieChart
+                data={cities}
+                theme="blues"
+              />
+            }
+          </Paper>
         </Grid>
-        <Grid item xs={12} sm={12} md={4}>
-          <Paper className={classes.paper}>6</Paper>
+        <Grid item xs={12} sm={12} md={6}>
+          <Paper className={classes.paper} style={{ height: 450 }}>
+            {age.length &&
+              <PieChart
+                data={age}
+                theme="oranges"
+              />
+            }
+          </Paper>
         </Grid>
-        <Grid item xs={12} sm={12} md={4}>
-          <Paper className={classes.paper}>7</Paper>
-        </Grid>
-        <Grid item xs={12} sm={12} md={4}>
-          <Paper className={classes.paper}>8</Paper>
+        <Grid item xs={12} sm={12} md={6}>
+          <Paper className={classes.paper} style={{ height: 450 }}>
+            {contract.length &&
+            <PieChart
+              data={contract}
+              theme="reds"
+            />
+            }
+          </Paper>
         </Grid>
       </Grid>
     </div>
